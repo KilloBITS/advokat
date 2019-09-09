1c06858eec20a24444685dca74924547ac9fc463
mr.kalinuk <qazwsx159357>
2019-09-09 17:25:23 +0300

    19. create auth page

--- src/components/head.js
+++ src/components/head.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import Logotype from './includes/logotype';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -12,11 +11,6 @@ class HeadComponent extends React.Component {
       </div>
       <div className="big_logotype">
         {(this.props.config !== null && this.props.config !== undefined)?<Logotype domId="Logotype" logotype={this.props.server + "/images/" +this.props.config.logo} logoColor={this.props.config.logoColor}/>:null}
-        <Link to={'/return'}>
-          <div className="gold_button">
-            Повернення 1% пенсійного збору сплаченого при купівлі квартири:
-          </div>
-        </Link>
       </div>
       <div id="scroll-d"><FontAwesomeIcon icon={['fas', 'arrow-down']} /></div>
     </div>
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -6,11 +6,19 @@ class SignUpPage extends React.Component {
     document.getElementById('page').scrollTo(0, 0);
   }
   render() {
-    return <div className="page signup">
+    return <div className="page signin">
       <MenuContainer transparent={false}/>
       <div className="pageContent">
-        <div className="pageSignUpContainer">
-
+        <div className="pageSignInContainer">
+          <div className="signInLine">
+            <input type="text" name="solister_login" className="signInInput"/>
+          </div>
+          <div className="signInLine">
+            <input type="password" name="solister_pass" className="signInInput"/>
+          </div>
+          <div className="signInLine">
+            <input type="button" className="signInButton" value="Вхід"/>
+          </div>
         </div>
       </div>
     </div>
