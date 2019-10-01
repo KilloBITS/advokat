a608170532583f106e3bab65593b2f723cbe0854
mr.kalinuk <qazwsx159357>
2019-10-01 21:18:55 +0300

    28. Fix admin

--- src/components/news.js
+++ src/components/news.js
@@ -9,7 +9,7 @@ import '../styles/swiper.css';
 
 let parseNews = (data, location, click, adm, removeNews) => {
   const dataBlock = data.map((comp, key) => <div key={key} className="newsBlockContent">
-    {(!adm)?<div className="remove_news" ai={comp.AI} onClick={removeNews}>Видалити</div>:null}
+    {(adm)?<div className="remove_news" ai={comp.AI} onClick={removeNews}>Видалити</div>:null}
     <div className="newsLine image">
       <img src={location + '/images/news/' + comp.image} alt=""/>
     </div>
@@ -158,8 +158,8 @@ class NewsComponent extends React.Component {
       {(this.state.modal)?<Modal open={this.state.modal} name={this.state.contentModal.title} text={this.state.contentModal.text} closeModal={this.closeModal}/>:null}
       <div className="isPage miniTitle">Останні новини</div>
       <div className="carouselBlock">
-      {(!this.props.admin)?<div className="add_blog" onClick={this.openCloseAddModal}>{(this.state.openetModal)?"Закрити":"Додати новину"}</div>:null}
-      {(!this.props.admin && this.state.openetModal)
+      {(this.props.admin)?<div className="add_blog" onClick={this.openCloseAddModal}>{(this.state.openetModal)?"Закрити":"Додати новину"}</div>:null}
+      {(this.props.admin && this.state.openetModal)
         ?<div className="addBlocgMOdal">
           <div className={(this.state.isLoaderModal)?"modalLoader show":"modalLoader"}></div>
           <div className="title_addMOdal">Додати новину</div>
